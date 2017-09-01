44a16cf251069818af15fd38c175b6ff8726dd33
fangzhiyuan <fzyiphone@sina.com>
2017-09-01 14:38:16 +0800

    Initial Commit

---
+++ labelStrLong/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  labelStrLong
+//
+//  Created by 威盛电气 on 2017/9/1.
+//  Copyright © 2017年 fzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
